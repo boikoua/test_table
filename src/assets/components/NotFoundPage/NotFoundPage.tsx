384f4dba10beb83e6c5417e156914a76c4a2d570
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-02-02 13:04:50 +0200

    NotFoundPage component

--- src/assets/components/NotFoundPage/NotFoundPage.tsx
+++ src/assets/components/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,12 @@
 import styles from './NotFoundPage.module.scss';
+import image from './../../img/not-found.png';
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <section className={styles.page}>
+      <img src={image} alt="Not Found image" />
+    </section>
+  );
 };
 
 export default NotFoundPage;
