00cb2aaeb5d02c455e33c9a7c08c0eae25b47d68
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-02-02 13:20:09 +0200

    Search realisation

--- src/assets/components/ProductList/ProductList.tsx
+++ src/assets/components/ProductList/ProductList.tsx
@@ -1,11 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import ProductItem from '../ProductItem';
 import styles from './ProductList.module.scss';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchItems } from '../../store/features/itemsSlice';
 import { AppDispatch, RootState } from '../../store/store';
 import Loader from '../Loader';
 import ProductsTable from '../ProductsTable';
+import { useSearchParams } from 'react-router-dom';
 
 const ProductList = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -13,13 +14,29 @@ const ProductList = () => {
     (state: RootState) => state.items
   );
 
+  const [searchValue, setSearchValue] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+
   useEffect(() => {
     dispatch(fetchItems());
   }, [dispatch]);
 
-  const showItems = items.map((item) => (
-    <ProductItem key={item.Id} item={item} />
-  ));
+  useEffect(() => {
+    const query = searchParams.get('query') || '';
+    setSearchValue(query);
+  }, [searchParams]);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchValue(value);
+    setSearchParams({ query: value });
+  };
+
+  const showItems = items
+    .filter((item) =>
+      item.Name.toLowerCase().includes(searchValue.toLowerCase())
+    )
+    .map((item) => <ProductItem key={item.Id} item={item} />);
 
   return (
     <section className={styles.products}>
@@ -30,7 +47,13 @@ const ProductList = () => {
       </p>
 
       <div className={styles.search}>
-        <input className={styles.input} type="text" placeholder="Search" />
+        <input
+          className={styles.input}
+          value={searchValue}
+          onChange={handleSearchChange}
+          type="text"
+          placeholder="Search"
+        />
         <button className={styles.btn}>
           <span className="icon-search"></span>
         </button>
